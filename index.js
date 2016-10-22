c9d12b353491eb4c0bf2d0f17733a18f2714d8d7
cezar teste <cezar teste>
2016-10-22 21:59:28 -0200

    - Teste 1

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-var http = require('http');
-var port = process.env.PORT;
-var http = require('http');
-var myserver  = http.createServer(function (req,res)
-{
-	console.log('algo', port);	
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
-myserver.listen(port, function () 
-{
-  console.log('Server listening at port %d', port);	
-});
-
-var io = require('socket.io')(myserver);
-
-/*app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});*/
-
-
