1645a6cbd60eba66cebeb3f237c71187d51228b7
cezar teste <cezar teste>
2016-10-22 18:09:59 -0200

    - Reload

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var express = require('express');
+var app = express();
 
-app.get('/', function(req, res){
-  res.render('Teste');
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
-app.use(function(err, req, res, next) {
-  res.send(err.stack);
-});
-
-/* istanbul ignore next */
-if (!module.parent) {
-  var port = process.env.PORT || 3000;
-  app.listen(port);
-  console.log('Express started on port 3000');
-}
