3f52086e5875dfdd3b0732d9497bd6ac36a566cf
cezar teste <cezar teste>
2016-10-22 17:58:37 -0200

    - Teste

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-var net = require('net');
-var vr_sockets = [];
-var vi_servidor = '127.0.0.1';
-var dados = "";
-var http = require('http');
-var myserver  = http.createServer(function (req,res)
-{
-	res.writeHead(200, {'Content-Type'   : 'text/plain','Access-Control-Allow-Origin' : '*'});
-	/*req.on('data',function(data)
-	{
-		console.log(data); 		
-	});
-		
-	for (var j=0; j < vr_sockets.length; j++)
-	{
-		dados += " Socket"+ j +"";
-	}
-	
-	req.on('end', function()
-	{			
-		console.log(dados); 		
-	});*/
-	res.end(dados); 
-});
-var port_number = myserver.listen(process.env.PORT || 3000);
+var express = require('express');
 
-net.createServer(function(sock)
-{   		
-	sock.on('data', function(data)
-	{
-		console.log('colocando socker na lista');
-		vr_sockets.push(sock);
-	});
-}).listen(6161,vi_servidor);
+app.get('/', function(req, res){
+  res.render('Teste');
+});
+
+
+app.use(function(err, req, res, next) {
+  res.send(err.stack);
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  var port = process.env.PORT || 3000;
+  app.listen(port);
+  console.log('Express started on port 3000');
+}
