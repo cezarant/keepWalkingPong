c63d0326356440c6c91debc9f11f743c3d16da18
cezar teste <cezar teste>
2016-10-22 21:10:36 -0200

    - App Carregando

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
