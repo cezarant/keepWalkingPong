079669a12c3fa1e5d5ed3b834d2c7e9c24a79399
cezar teste <cezar teste>
2016-10-22 20:53:21 -0200

    - Teste subindo socket

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+/*var io = require('socket.io')(server);
 var port = process.env.PORT || 5000;
 
 server.listen(port, function ()
@@ -11,7 +11,7 @@ server.listen(port, function ()
 });
 
 // Routing
-/*app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // Chatroom
 
