a2b24561a7de711c5ce0faa11650cc82efd98ff5
cezar teste <cezar teste>
2016-10-22 21:23:39 -0200

    - Teste

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-app.set('port', (process.env.PORT || 5000));
+/*app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,6 +18,6 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
+});*/
 
 
