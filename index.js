88452875fc23e1ee4848386267647fe449dbf94d
cezar teste <cezar teste>
2016-10-22 18:25:29 -0200

    - HTTP

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-var express = require('express');
-var net = require('net');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('Estou aqui');
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<!DOCTYPE "html">");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
 });
 
-/*app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});*/
-
-net.createServer(function(sock)
-{   		
-	sock.on('data', function(data)
-	{
-		console.log('colocando socker na lista');
-		vr_sockets.push(sock);
-	});
-}).listen(6161,vi_servidor);
+server.listen(80);
+console.log("Server is listening");
