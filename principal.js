b1ea97ae8073cea0c7badea711390ab47c9dbec6
cezar teste <cezar teste>
2016-10-23 14:53:03 -0200

    - Retirando código desnecessario da Página principal

--- principal.js
+++ principal.js
@@ -1,54 +1,30 @@
 $(function() {
-  var FADE_TIME = 150; // ms  
-  var COLORS = [
-    '#e21400', '#91580f', '#f8a700', '#f78b00',
-    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-  ];
-
-  // Initialize variables
-  var $window = $(window);
-  var $usernameInput = $('#txtLogin'); // Input for username
-  var $messages = $('.messages'); // Messages area
- 
-  var $btnSubir = $('#btnSubir');
-  var $btnDescer = $('#btnDescer');
-  
-  var $chatPage = $('.chat.page'); // The chatroom page
-  // Prompt for setting a username
+  var FADE_TIME = 150; // ms      
+  var $window = $(window);  
+  var $messages = $('.messages');   
+  var $chatPage = $('.chat.page');   
   var username;
   var connected = false;
   var typing = false;
-  var lastTypingTime;
-  var $currentInput = $usernameInput.focus();
-
+  var lastTypingTime;  
   var socket = io();
-
-  function addParticipantsMessage (data) {
+ 
+  function addParticipantsMessage (data)
+  {
     var message = '';
-    if (data.numUsers === 1) {
+    if (data.numUsers === 1)
+	{
       message += "there's 1 participant";
     } else {
       message += "there are " + data.numUsers + " participants";
     }
     //log(message);
   }
-
   function ativarMonitoramento()
   {
-	$chatPage.show();	  
-	
+	$chatPage.show();	  	
   }  
-  // Sets the client's username
-  function setUsername()
-  {
-    username = cleanInput($usernameInput.val().trim());    
-    if (username) 
-	{      
-      socket.emit('add user', username);
-    }
-  }
-
+  
   function sendMessage()
   {
     var message = 'Teste';    
@@ -82,7 +58,7 @@ $(function() {
 
     var $usernameDiv = $('<span class="username"/>')
       .text(data.username)
-      .css('color', getUsernameColor(data.username));
+      .css('color', '#000000');
     var $messageBodyDiv = $('<span class="messageBody">')
       .text(data.message);
 
@@ -157,49 +133,8 @@ $(function() {
     return $('.typing.message').filter(function (i) {
       return $(this).data('username') === data.username;
     });
-  }  
-  function getUsernameColor (username) 
-  {
-    // Compute hash code
-    var hash = 7;
-    for (var i = 0; i < username.length; i++) {
-       hash = username.charCodeAt(i) + (hash << 5) - hash;
-    }
-    // Calculate color
-    var index = Math.abs(hash % COLORS.length);
-    return COLORS[index];
-  }
-  
-  $window.keydown(function (event) 
-  {
-    // Auto-focus the current input when a key is typed
-    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-      $currentInput.focus();
-    }
-    // When the client hits ENTER on their keyboard
-    if (event.which === 13) {
-      if (username) {
-        sendMessage();
-        socket.emit('stop typing');
-        typing = false;
-      } else {
-        setUsername();
-      }
-    }
-  });
-
-  $btnSubir.on('click',function()
-  {
-	subir();  
-  });
-  
-  $btnDescer.on('click',function()
-  {
-	descer();  
-  });
-  
-  
-  
+  }    
+   
   socket.on('login', function (data)
   {
     connected = true;
@@ -243,22 +178,5 @@ $(function() {
   {    
     data.message = statusOfBar;
     addChatMessage(data);
-  }
-  // Removes the visual chat typing message
-  function mostraDescendo(data) 
-  {
-    getTypingMessages(data).fadeOut(function () {
-      $(this).remove();
-    });
-  }  
-  /****************************************************************/
-  
-  socket.on('typing', function (data)
-  {
-    addChatTyping(data);
-  });
-  
-  socket.on('stop typing', function (data) {
-    removeChatTyping(data);
-  });
+  }      
 });
