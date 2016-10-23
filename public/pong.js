8d8a1b823059fb3e337c9f20a3b9f2f9ae058096
cezar teste <cezar teste>
2016-10-23 00:12:32 -0200

    - Funções de subir e descer

--- public/pong.js
+++ public/pong.js
@@ -1,6 +1,5 @@
 $(function() {
-  var FADE_TIME = 150; // ms
-  var TYPING_TIMER_LENGTH = 400; // ms
+  var FADE_TIME = 150; // ms  
   var COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
@@ -17,7 +16,6 @@ $(function() {
 
   var $loginPage = $('.login.page'); // The login page
   var $chatPage = $('.chat.page'); // The chatroom page
-
   // Prompt for setting a username
   var username;
   var connected = false;
@@ -39,26 +37,20 @@ $(function() {
 
   // Sets the client's username
   function setUsername () {
-    username = cleanInput($usernameInput.val().trim());
-
-    // If the username is valid
-    if (username) {
+    username = cleanInput($usernameInput.val().trim());    
+    if (username) 
+	{
       $loginPage.fadeOut();
       $chatPage.show();
-      $loginPage.off('click');
-      
-
-      // Tell the server your username
+      $loginPage.off('click');      
       socket.emit('add user', username);
     }
   }
 
-  // Sends a chat message
-  function sendMessage () {
-    var message = 'Teste';
-    // Prevent markup from being injected into the message
-    message = cleanInput(message);
-    // if there is a non-empty message and a socket connection
+  function sendMessage()
+  {
+    var message = 'Teste';    
+    message = cleanInput(message);    
     if (message && connected)
 	{      
       addChatMessage({
@@ -69,15 +61,15 @@ $(function() {
       socket.emit('new message', message);
     }
   }
-
   // Log a message
-  function log (message, options) {
+  function log (message, options)
+  {
     var $el = $('<li>').addClass('log').text(message);
     addMessageElement($el, options);
   }
-
   // Adds the visual chat message to the message list
-  function addChatMessage (data, options) {
+  function addChatMessage (data, options)
+  {
     // Don't fade the message in if there is an 'X was typing'
     var $typingMessages = getTypingMessages(data);
     options = options || {};
@@ -100,31 +92,25 @@ $(function() {
 
     addMessageElement($messageDiv, options);
   }
-
   // Adds the visual chat typing message
-  function addChatTyping (data) {
+  function addChatTyping (data) 
+  {
     data.typing = true;
     data.message = 'is typing';
     addChatMessage(data);
   }
-
   // Removes the visual chat typing message
   function removeChatTyping (data) {
     getTypingMessages(data).fadeOut(function () {
       $(this).remove();
     });
   }
-
-  // Adds a message element to the messages and scrolls to the bottom
-  // el - The element to add as a message
-  // options.fade - If the element should fade-in (default = true)
-  // options.prepend - If the element should prepend
-  //   all other messages (default = false)
-  function addMessageElement (el, options) {
-    var $el = $(el);
-
-    // Setup default options
-    if (!options) {
+ 
+  function addMessageElement (el, options)
+  {
+    var $el = $(el);    
+    if (!options) 
+	{
       options = {};
     }
     if (typeof options.fade === 'undefined') {
@@ -144,10 +130,9 @@ $(function() {
       $messages.append($el);
     }
     $messages[0].scrollTop = $messages[0].scrollHeight;
-  }
-
-  // Prevents input from having injected markup
-  function cleanInput (input) {
+  }  
+  function cleanInput (input)
+  {
     return $('<div/>').text(input).text();
   }
   /** Up and Down */
@@ -156,44 +141,23 @@ $(function() {
   function subir()
   {	
 	if (connected)    
-        socket.emit('typing');
+        socket.emit('subir');
   }
   /** Function to down the bar */
   function descer()
   {
 	 if (connected) 	        
-        socket.emit('stop typing');
+        socket.emit('descer');
   }  
-  /*****************************************************************************************/  
-  // Updates the typing event
-  function updateTyping () {
-    if (connected) {
-      if (!typing) {
-        typing = true;
-        socket.emit('typing');
-      }
-      lastTypingTime = (new Date()).getTime();
-
-      setTimeout(function () {
-        var typingTimer = (new Date()).getTime();
-        var timeDiff = typingTimer - lastTypingTime;
-        if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
-          socket.emit('stop typing');
-          typing = false;
-        }
-      }, TYPING_TIMER_LENGTH);
-    }
-  }
-
-  // Gets the 'X is typing' messages of a user
-  function getTypingMessages (data) {
+  /*****************************************************************************************/    
+  function getTypingMessages (data) 
+  {
     return $('.typing.message').filter(function (i) {
       return $(this).data('username') === data.username;
     });
-  }
-
-  // Gets the color of a username through our hash function
-  function getUsernameColor (username) {
+  }  
+  function getUsernameColor (username) 
+  {
     // Compute hash code
     var hash = 7;
     for (var i = 0; i < username.length; i++) {
@@ -203,10 +167,9 @@ $(function() {
     var index = Math.abs(hash % COLORS.length);
     return COLORS[index];
   }
-
-  // Keyboard events
-
-  $window.keydown(function (event) {
+  
+  $window.keydown(function (event) 
+  {
     // Auto-focus the current input when a key is typed
     if (!(event.ctrlKey || event.metaKey || event.altKey)) {
       $currentInput.focus();
@@ -233,7 +196,8 @@ $(function() {
 	descer();  
   });
   
-  $loginPage.click(function () {
+  $loginPage.click(function() 
+  {
     $currentInput.focus();
   });
   
@@ -266,13 +230,35 @@ $(function() {
     addParticipantsMessage(data);
     removeChatTyping(data);
   });
-
-  // Whenever the server emits 'typing', show the typing message
-  socket.on('typing', function (data) {
+  /****************************************************************/
+  /** Socket functions  */
+  socket.on('subir', function (data)
+  {
+     alteraStatus(data,'Subindo barra'); 
+  });  
+  socket.on('descer', function (data)
+  {
+    alteraStatus(data,'Descendo barra'); 
+  });
+  function alteraStatus(data,statusOfBar) 
+  {    
+    data.message = statusOfBar;
+    addChatMessage(data);
+  }
+  // Removes the visual chat typing message
+  function mostraDescendo(data) 
+  {
+    getTypingMessages(data).fadeOut(function () {
+      $(this).remove();
+    });
+  }  
+  /****************************************************************/
+  
+  socket.on('typing', function (data)
+  {
     addChatTyping(data);
   });
-
-  // Whenever the server emits 'stop typing', kill the typing message
+  
   socket.on('stop typing', function (data) {
     removeChatTyping(data);
   });
