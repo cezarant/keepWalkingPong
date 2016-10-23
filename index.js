33eb17decafb6d46a4f3279fafbca3f7c318121e
cezar teste <cezar teste>
2016-10-22 22:02:34 -0200

    - Carga

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ var port = process.env.PORT || 3000;
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
+
+// Routing
+app.use(express.static(__dirname + '/public'));
