753de750919678ca893a26fb1159287723341989
cezar teste <cezar teste>
2016-10-23 15:25:37 -0200

    - Don't permit more of two users

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ io.on('connection', function (socket)
   });
 
   // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username){
-    if (addedUser) return;
-
+  socket.on('add user', function (username)
+  {
+    if ((addedUser) || (numUsers === 2)) return;
     // we store the username in the socket session for this client
     socket.username = username;
     ++numUsers;
